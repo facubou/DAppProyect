26c7872e59bf14dea9fc6af7cf06516e25ed94f7
Facundo Bouza <facundo.bouza@gmail.com>
2022-05-09 22:50:39 -0300

    creating client side

--- contracts/TaskContract.sol
+++ contracts/TaskContract.sol
@@ -5,6 +5,11 @@ contract TaskContract
 
     uint taskCounter = 0;
 
+    constructor()
+    {
+        createTask("1er tarea","tengo que hacer algo");
+    }
+
     struct Task{
         uint id;
         string title;
@@ -12,12 +17,39 @@ contract TaskContract
         bool done;
         uint createdAt;
     }
+
+    event TaskCreated(
+        uint id,
+        string title, 
+        string description, 
+        bool done,
+        uint createdAt
+    );
+
+    event TaskCambiaDone(
+        uint id,
+        bool done
+    );
+
     mapping (uint => Task) public tasks; 
 
     function createTask(string memory _title, string memory _description) public
     {
-        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
         taskCounter++;
+        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
+        //emit se utiliza para almacenar logs utilizando eventos
+        emit TaskCreated(taskCounter, _title, _description, false, block.timestamp);
+        
+    }
+
+    function changeDone(uint _id) public 
+    {
+        //copio la tarea completa y luego cambio el valor de done
+        Task memory _task = tasks[_id];
+        _task.done = !_task.done;
+        tasks[_id] = _task;
+        emit TaskCambiaDone(_id, _task.done);
+
     }
 
 }
